e0e1f5dc483bc97d2c0f4434e06890d6dccf7c52
dongyeon4 <152827939+dongyeon4@users.noreply.github.com>
2023-12-11 11:03:23 +0900

    Add files via upload

---
+++ tt1.py
@@ -0,0 +1,286 @@
+import matplotlib.pyplot as plt
+import streamlit as st
+ 
+fig, ax = plt.subplots()
+
+c1 = st.sidebar.radio('선의 색을 선택하십시오',['red','green','blue','orange'])
+s1 = st.sidebar.radio('선의 형태를 선택하십시오',['-',':','-.','--'])
+m1 = st.sidebar.radio('마커스의 스타일을 선택하시오',['o','^','s','p'])
+fig, ax = plt.subplots()
+
+love = []
+y = []
+for i in range (-20,21,2):
+    love.append(i)
+    y.append(-2*i*i+3*i+5)
+
+    plt.plot(love,y,color=c1, linestyle=s1, marker=m1)
+    st.pyplot(fig)
+
+
+
+
+
+
+
+
+
+
+# import streamlit as st
+
+
+
+# import matplotlib.pyplot as plt
+# import numpy as np 
+
+# c1 = st.radio('선택하십시오'),['red','green' 'blue' 'orange']
+
+# fig ax = plt.
+# import time
+# import random as r
+
+# import turtle
+
+# import streamlit as st
+
+# import matplotlib. pyplot as plt
+
+
+
+# fig, ax = plt.subplots()
+
+# x = []
+# y= []
+# ysin = []
+# for i in range(-100,100)
+#     x.append(i/10.0)
+#     y.append(i)   
+#     ysin.append(1200*np. sin i)
+    
+
+#     plt.plot(x. y, 'rs' , label = '2nd equation')
+#     plt.plot
+
+
+# x 
+
+
+        
+
+
+# s1 = 1
+# s1 =2
+# s1 =3
+# s1 =4
+# s1 =5
+# s1, s2, s3, s4, s5
+
+# s = ['a', 'b', 'c', 'd', 'e'] 
+
+# s = [3, 7, 1, 15, -6 ,8 ,9]
+# s
+
+# for i in s:
+#     t = t + i
+# t
+    
+
+# t=turtle. Turtle()
+
+# # t.speed(0)
+# # t.pensize(5)
+# # t.goto(0,0)
+# # while True:
+# #     for i in range(30):
+# #         t.circle(1+5*i)
+# #         t.color
+# for j in range(4):
+    
+# t.forward(1+ 5*i)
+# t.left(90)
+# t.forward(1+ 5*i)
+# t.left(90)
+# t.forward(1+ 5*i)
+# t.left(90)
+# t.forward(1+ 5*i)
+# t.left(90)
+# t.shape('turtle')
+# t.color((77/255, 239/255, 93/255))
+
+# t.forward()               
+
+# turtle.done
+
+
+
+
+
+
+
+
+
+
+
+
+
+# t1 = turtle.turtles()
+# t2 = turtle.turtles()
+
+# t1.shape('turtle')
+# t2.shspe('turtle')
+
+# t1.penup()
+# t1.shapesize(3)
+# t1.goto(-300,100)
+
+# t2.penup()
+# t1.shapesize(3)
+# t2.goto(-300,-100)
+
+# for i in range(50):
+#     di = r.randint(1,30)
+#     t1=forward(d1)
+#     d2=r.randint(1,30)
+#     t2.forward(d2)
+
+
+# for i in range(6):
+# al = random.randint(1, 45)
+# a2= r.random()
+# a1, a2
+
+# a=0
+# for i in range(100):
+# r.choice('abcdef')
+# if c == 'a':
+#     a = a + 1
+
+#     a/n, % 
+
+
+
+# import turtle 
+# t = turtle.Turtle()
+# t.shape('turtle')
+# t.speed(1)
+
+# def square(x, y, lx, ly)
+#     t.up()
+#     t.goto(x, y)
+#     t.down()
+#     for i in range(4):
+#         t.forward(lx)
+#         t.left(90)
+#         t.forward(ly)
+#         t.left(90)
+
+#         Rec(-200, 0, 100, 50)
+#         Rec(0, 0, 100, 150)
+#         Rec(200, 0, 100, 100)
+#         turtle.done()
+# t = turtle. turtles()
+# t.shape('turtle')
+# t.speel(1)
+
+# turtle. done()
+# for i in range(1, 10) :
+#     if i%3 ==1:
+#         i
+
+
+# s = 0
+# for i in range(10,21):
+#      s = s+i
+# s 
+
+# def user_sum(n):
+#     s = 0
+#     for i in range(1, n+1):
+#         s = s+i
+#       s 
+# user_sum(100)
+# user_sum(200)
+
+
+
+
+
+
+
+# s = 50
+# if s >= 60:
+#     st.write('귀하의 점수는' + str(r)+ '점으로 :blue[A등급]입니다')
+
+# elif s >= 80:
+#     '귀하의 점수는' + str(s)+ '점으로 :green[B등급]입니다
+
+
+# elif s >= 70:
+#     '귀하의 점수는' + str(s)+ '점으로 :orange[c등급]입니다
+
+#  elif s >= 60:
+#     '귀하의 점수는' + str(s)+ '점으로 :blue[D등급]입니다
+
+#     elif:
+#     '귀하의 점수는' + str(s)+ '점으로 :red[F등급]입니다
+
+
+
+
+
+
+
+
+
+
+# import turtle
+
+# r = 20
+# area = 3.14*r*r
+# area 
+
+
+
+# t = turtle.Turtle()
+# t.shape('turtle')
+
+# s=100
+# t.left(60)
+# t.forward(100)
+# t.right(120)
+# t.forward(100)
+# t.right(120)
+# t.forward(100)
+# s+300
+# t.right(30)
+# t.forward(s)
+# t.right(90)
+# t.forward(s)
+# t.right(90)
+# t.forward(s)
+# t.right(90)
+# t.forward(s)
+# turtle.done()
+
+# f = 50
+# r = 80
+# t.speed(2)
+# t.circle(r)
+# t.fd(f)
+# t.circle(r)
+# t.fd(f)
+# t.circle(r)
+# turtle.done()
+
+
+
+# st.write('스트림릿')
+
+# st.image('im.jfif')
+
+
+# print('Hellow')
+
+# st.write('Hello')
+# st.write('강아지'+'고양이')
+
